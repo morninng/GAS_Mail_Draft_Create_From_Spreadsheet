15fcb98dc46903abeb4b7ffb92afe3c6a3041b76
Yuta Moriyama <mr_morninng_star_dash@yahoo.co.jp>
2024-12-09 13:08:37 +0900

    use another pc

--- src/mail_simple.ts
+++ src/mail_simple.ts
@@ -26,12 +26,12 @@ export class MailSimple {
     // (1)Spreadsheetファイルを開く
     const SHEET_URL =
       "https://docs.google.com/spreadsheets/d/1kkc4lrEVd-WPXryMKVvG1mxz83PG3fwACPKGHSqvrAA/edit#gid=0";
-    const SHEET_NAME = "送信時調節２";
+    const SHEET_NAME = "送信調節３";
     var spreadSheet = SpreadsheetApp.openByUrl(SHEET_URL);
     // (2)Sheetを開く
     var sheet = spreadSheet.getSheetByName(SHEET_NAME);
     // (3)セルの範囲を指定・(4)値の取得
-    var table = sheet.getRange('H1:J1380').getValues();
+    var table = sheet.getRange('H1:J1730').getValues();
     // 結果を表示
     Logger.log(table);
     let length = 0;
@@ -56,7 +56,7 @@ export class MailSimple {
   createMailDraft(userEmail, shoolName, userName) {
 
     const content = this.createMailContent(shoolName, userName)
-    const title = "英語ディベート大会 @Tokyo  7月14日(日)初心者 7月15日(祝) のアナウンス"
+    const title = "英語ディベート大会 @Tokyo  1月19日(日)経験者 3月2日(日)初心者 のアナウンス"
 
     this.createMailDraftExecute(userEmail, title, content )
   }
@@ -66,36 +66,36 @@ export class MailSimple {
     return `${shoolName} ${userName}様
 
     英語ディベート大会に参加頂きたくご連絡させていただきました。
-    
-    
-    7月14日 (日曜) 小学生中学生のディベート初心者大会
-    https://docs.google.com/document/d/1V_drc29vaWnDFE6krmlmRiuNkRpvZ5afjBhZF-X5LKU/edit
-    
-    7月14日 (日曜) ：小学生英語ディスカッション
-    https://docs.google.com/document/d/1CkKpQ9ybFXZe7NqFCvwtz90q38hQbMSXRt9e2DPw8TA/edit
-    
-    7月15日  (月曜・祝日)：中高生のディベート経験者向け大会
-    https://docs.google.com/document/d/1sucGOvXZZ_1NnbIwW-Dty1RBrn_pRmPOBFgk6-chx7Y/edit
-    
+
+
+  経験者大会：　2025年 1月19日  (日曜)
+  https://docs.google.com/document/d/1Ap_Z7R_ZjDKE94uBFWvO-spX4Yat1N5bu-6eQX3lOfA/edit?tab=t.0
+
+  初心者大会： 2025年3月2日 (日曜)
+  https://docs.google.com/document/d/1WNz2aEPeP-NrQVLl8UEDt_vCypFbqWzcWhUCfCGUYXc/edit?tab=t.0
+
+  哲学対話：  2025年3月2日  (日曜)
+  https://docs.google.com/document/d/1ZKoQ_uOmkNZNUeklyYaf0fwUldAoZxmJZ-2GwNMmIz4/edit?tab=t.0
+
 
     [特徴]
     - 提供ジャッジ不要で何チームでも参加できるので、普段大会に出場できる機会が無い方でも参加できます
     - 初心者大会は順位よりも楽しむのを重要視して運営いたします
     - レベル分けを厳密に行うので、実力差がありすぎる相手と対戦することはありません
     - 個人での申し込みが可能です
-    
+
 
     質問はこちらのメールか
     Line:  https://line.me/ti/p/8m97k13AO6
     に質問いただけると嬉しいです
-  
-    
+
+
     何卒よろしくお願いいたします
-    
+
     運営一同`
 
   }
-  
+
 
   public createMailDraftExecute(userEmail, title, content ) {
 
@@ -103,7 +103,6 @@ export class MailSimple {
     if(userEmail){
       GmailApp.createDraft( userEmail, title, content, objArgs );
     }
- 
+
   }
 }
-
